1ac073394905ef4789c7e370e1298e7f29396835
geeta <geeta@mykumpany.com>
2021-03-23 01:14:32 +0530

    wrk

--- src/android/BluetoothPluginWrapper.java
+++ src/android/BluetoothPluginWrapper.java
@@ -84,9 +84,8 @@ private String deviceMACAddress, deviceName, device_MAC_Add, selectedUSBDevice, 
 				// // intent.putExtra(MAC_ADDRESS, "38:3C:9C:EA:9F:73");
 				// // intent.putExtra(DEVICE_COMMUNICATION_MODE, "BLUETOOTHCOMMUNICATION");
 				// // intent.putExtra("cashBackAmoumt", "");
-				 initialization = new PaymentInitialization(
-                        PaymentTransactionActivity.this);
-                initialization.initiateTransaction(handler,"C-ME30S-099184",
+				 initialization = new PaymentInitialization();
+                initialization.initiateTransaction("","C-ME30S-099184",
                         "38:3C:9C:EA:9F:73",
                         "10",
                         "BalanceEnquiry",
